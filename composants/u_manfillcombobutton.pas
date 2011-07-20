18c0c492f593ef3afdcc7bd1233397740e9b5987
girouxmatthieu <girouxmatthieu@localhost>
2011-07-20 16:10:49 +0200

    Integrating Fill Combo Button, reviewing source in function

---
+++ composants/u_manfillcombobutton.pas
@@ -0,0 +1,56 @@
+unit u_manfillcombobutton;
+
+{$IFDEF FPC}
+{$mode Delphi}{$H+}
+{$ENDIF}
+
+interface
+
+uses
+  Classes, SysUtils, Forms,
+  {$IFDEF VERSIONS}
+     fonctions_version,
+  {$ENDIF}
+  u_fillcombobutton, Graphics;
+
+{$IFDEF VERSIONS}
+const
+    gVer_ManFillCombo : T_Version = ( Component : 'Bouton personnalisé de remplissage de combo box avec lien de fonction enregistrée' ;
+                                       FileUnit : 'u_manfillcombobutton' ;
+                                       Owner : 'Matthieu Giroux' ;
+                                       Comment : 'Composant bouton de remplissage de lien 1-N avec lien de fonction enregistrée.' ;
+                                       BugsStory : '0.8.0.0 : Not Finished.';
+                                       UnitType : 3 ;
+                                       Major : 0 ; Minor : 8 ; Release : 0 ; Build : 0 );
+{$ENDIF}
+
+{ TFWFillCombo }
+type
+  TManFillCombo = class ( TFWFillCombo )
+     private
+      FFormCode : String;
+     protected
+      procedure CreateForm(const aico_Icon: TIcon); override;
+     published
+      property FormCode : String read FFormCode write FFormCode;
+    End;
+
+implementation
+
+uses fonctions_Objets_Data;
+
+{ TFWFillCombo }
+
+procedure TManFillCombo.CreateForm(const aico_Icon: TIcon);
+begin
+  FFormModal := ffor_ExecuteFonction ( FFormCode, False );
+
+end;
+
+
+{$IFDEF VERSIONS}
+initialization
+  p_ConcatVersion ( gVer_ManFillCombo  );
+{$ENDIF}
+end.
+
